8cdb722b73dc70c3983442bc95a484fcc95a1ac4
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-28 23:40:10 -0500

    test: add case for node ID uniqueness and insert data

--- NodeTest.java
+++ NodeTest.java
@@ -2,6 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class NodeTest {
+
     @Test
     public void test_node_IDUniqueness() {
         Node root = new Node(100);
@@ -11,5 +12,27 @@ public class NodeTest {
         assertNotEquals(root.getNodeID(), leftChild.getNodeID());
         assertNotEquals(leftChild.getNodeID(), rightChild.getNodeID());
         assertNotEquals(root.getNodeID(), rightChild.getNodeID());
+    }
+
+    @Test
+    public void testInsertData() {
+        Node root = new Node(10);
+
+        root.insert(5);
+        root.insert(15);
+        root.insert(3);
+        root.insert(7);
+        root.insert(12);
+        root.insert(20);
+
+        assertNotNull(root.getLeft());
+        assertNotNull(root.getRight());
+        assertEquals(5, root.getLeft().getData());
+        assertEquals(15, root.getRight().getData());
+
+        assertEquals(3, root.getLeft().getLeft().getData());
+        assertEquals(7, root.getLeft().getRight().getData());
+        assertEquals(12, root.getRight().getLeft().getData());
+        assertEquals(20, root.getRight().getRight().getData());
     }
 }
